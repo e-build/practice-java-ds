a76ee6a62b73c51868fe2b4ace6b77378b29651c
e-build <dongeol92@daum.net>
2024-04-01 00:19:50 +0900

    iterator add() refactoring

--- src/main/java/classic/linear/DoublyLinkedList.java
+++ src/main/java/classic/linear/DoublyLinkedList.java
@@ -204,32 +204,22 @@ public class DoublyLinkedList<T> implements Iterable<T> {
         }
 
         public void add(T e) {
-            // 반복자가 현재 가리키고 있는 next 요소 앞에 새 요소를 삽입합니다.
-            // 만약 next가 null이라면, 리스트의 끝에 추가하는 것과 같습니다.
-
-            // 새 노드의 이전 노드는 next의 이전 노드입니다. 처음에 추가하는 경우, 이전 노드는 null입니다.
             Node<T> prevNode = (next == null) ? tail : next.prev;
-            // 새 노드 생성. next가 null이 아니라면, next는 새 노드의 다음 노드입니다.
             Node<T> newNode = new Node<>(prevNode, next, e);
 
             if (prevNode == null) {
-                // 이전 노드가 null이면, 리스트의 처음에 추가하는 것입니다.
                 head = newNode;
             } else {
-                // 그렇지 않다면, 새 노드를 이전 노드의 다음 노드로 설정합니다.
                 prevNode.next = newNode;
             }
 
             if (next == null) {
-                // next가 null이라면, 리스트의 끝에 추가하는 것이므로, tail을 업데이트합니다.
                 tail = newNode;
             } else {
-                // next가 null이 아니라면, next의 이전 노드를 새 노드로 설정합니다.
                 next.prev = newNode;
             }
 
-            // 새 요소를 추가한 후, nextIndex를 증가시키고, lastReturned를 null로 설정합니다.
-            // 이는 add 연산 후에는 lastReturned가 더 이상 유효하지 않기 때문입니다.
+            size++;
             nextIndex++;
             lastReturned = null;
         }
